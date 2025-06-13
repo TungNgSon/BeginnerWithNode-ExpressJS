ef485c3adfc60b4abf9fedef4c6e31ffa5ebd9f1
TungNsSon <mmr9grs@gmail.com>
2025-06-13 22:13:20 +0700

    let all static route come first before dynamic route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const { createUser, getUsers,getAllUsers,loginUser } = require('../controllers/userController');
 const authenticate = require('../middlewares/auth');
 
-router.post('/register', createUser);   
-router.get('/:email', authenticate, getUsers);
-router.get('/', authenticate, getAllUsers); 
-router.post('/login', loginUser);  
-router.get('/profile', authenticate, (req, res) => {
-  res.json({ message: `Welcome, ${req.user.email}` });
-});
+
 //Register
 router.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'register.html'));
@@ -20,3 +14,10 @@ router.get('/login', (req, res) => {
   res.render('login', { title: 'Login' });
 });
 module.exports = router;
+router.post('/register', createUser);   
+router.get('/:email', authenticate, getUsers);
+router.get('/', authenticate, getAllUsers); 
+router.post('/login', loginUser);  
+router.get('/profile', authenticate, (req, res) => {
+  res.json({ message: `Welcome, ${req.user.email}` });
+});
